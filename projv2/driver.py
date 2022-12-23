68069f5b47b68d25a2fd8e69c1ff92bbb8e2126b
Noor-Nizar <88066634+Noor-Nizar@users.noreply.github.com>
2022-12-23 23:51:08 +0200

    Co-authored-by: Abdelrahman Ahmed <Boodz3@users.noreply.github.com> Co-authored-by: abdelrahmanhesham601@gmail.com <abdelrahmanhesham601@gmail.com>

--- projv2/driver.py
+++ projv2/driver.py
@@ -1,22 +1,25 @@
-# Import Pyke's engine module
+import contextlib
+import sys
+import time
 
 from pyke import knowledge_engine, krb_traceback, goal
-# Create a Pyke engine
-
+from pyke import ask_tty
 engine = knowledge_engine.engine(__file__)
-
-engine.activate('rules')
-
-my_goal = goal.compile('facts.CSIntern(T)')
-
-any = "T"
-# listp = my_goal.prove(engine, any = any)
-with my_goal.prove(engine,  T = any) as listp:
-    print(list(listp))
-    for vars in listp:
-        print(vars['T'])
-    # for vars in listp:
-    #     print("sdfa")
-    #     print(vars['T'])
-
-engine.print_stats()
+# Create a Pyke engine
+def test():
+    engine.reset()
+    engine.activate('rules')
+    print ("doing proof")
+    try :
+        with engine.prove_goal('facts.QualifiedForJob($job)') as gen :
+            for vars,plan in gen:
+                print ("-------------------")
+                print (vars['job'])
+    except Exception:
+        krb_traceback.print_exc()
+        sys.exit(1)
+    print ()
+    print ("done")
+    # engine.print_stats()
+    
+test()
